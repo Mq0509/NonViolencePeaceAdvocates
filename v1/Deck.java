096f56f508289c9381f4572cff364c66437f65ca
JefferyTang23 <jtang30@stuy.edu>
2022-05-27 14:11:15 -0400

    Deck and Table

--- v1/Deck.java
+++ v1/Deck.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 public class Deck {
-  ArrayList<Card> collection = new ArrayList<Card>();
+  private ArrayList<Card> collection;
+  private Card top;
   public Deck(){
+    collection = new ArrayList<Card>();
     for(int i = 0; i < 4; i++){ //adding color cards + special cards numbered from 1 thru 9
       for(int j = 1; j < 13; j++){
         collection.add(new Card(i,j));
@@ -18,4 +20,29 @@ public class Deck {
         collection.add(new Card(5,j));
     }
   }
+  public Deck(ArrayList asdf) {
+    collection = asdf;
+  }
+
+  public void swap(int card1, int card2) {
+    Card temp = collection.get(card2);
+    collection.swap(card2, collection.get(card1));
+    collection.swap(card1, temp);
+  }
+
+  public void shuffle() {
+    for(int c = 0; c < collection.size() ; c++) {
+      swap(c, (int) (Math.random*collection.size));
+    }
+  }
+
+  public Card draw() {
+    collection.remove();
+  }
+
+  public int getSize() {
+    return collection.size();
+  }
+
+  public void refill
 }
---
+++ v1/Table.java
@@ -0,0 +1,22 @@
+public class Table() {
+  private ArrayList<Card> placed = new ArrayList<Card>();
+  private Deck aDeck = new Deck();
+
+  public void addCard(Card used) {
+    placed.add(used);
+  }
+  public Card placeFirst() {
+    placed.add(aDeck.draw());
+    return placed[0];
+  }
+
+  public ArrayList<Card> getPlaced() {
+    return placed;
+  }
+
+  public Card refill() {
+    if (aDeck.getSize() == 0) {
+      
+    }
+  }
+}
