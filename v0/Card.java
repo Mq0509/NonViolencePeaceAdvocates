a89a8b39f0339725b765e009807b89f110268aa2
xinrachel30 <xlin30@stuy.edu>
2022-05-27 10:49:58 -0400

    oops

--- v0/Card.java
+++ v0/Card.java
@@ -5,4 +5,51 @@ public class Card{
     _color = color;
     _numberOrSpecialty = numberOrSpecialty;
   }
+
+  public String getColor() {
+    String color = "";
+    if (_color == 1) {
+      color = "Red";
+    }
+    else if (_color == 2) {
+      color = "Green";
+    }
+    else if (_color == 3) {
+      color = "Yellow";
+    }
+    else if (_color == 4) {
+      color = "Blue";
+    }
+    else {
+      color = "Wild";
+    }
+    return color;
+  }
+
+  public String getNumberOrSpecialty() {
+    String numberOrSpecialty = "";
+    if (_numberOrSpecialty >= 0 && _numberOrSpecialty <= 9) {
+      numberOrSpecialty += _numberOrSpecialty;
+    }
+    else if (_numberOrSpecialty == 10) {
+      numberOrSpecialty = "+2";
+    }
+    else if (_numberOrSpecialty == 11) {
+      numberOrSpecialty = "Reverse";
+    }
+    else if (_numberOrSpecialty == 12) {
+      numberOrSpecialty = "Cancel";
+    }
+    else if (_numberOrSpecialty == 13) {
+      numberOrSpecialty = "Regular";
+    }
+    else {
+      numberOrSpecialty = "+4";
+    }
+    return numberOrSpecialty;
+  }
+
+  public String toString() {
+    return getColor() + " " + getNumberOrSpecialty();
+  }
 }
--- v0/Deck.java
+++ v0/Deck.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 public class Deck {
   ArrayList<Card> collection = new ArrayList<Card>();
   public Deck(){
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < 4; i++){ //adding color cards + special cards numbered from 1 thru 9
       for(int j = 1; j < 13; j++){
         collection.add(new Card(i,j));
         collection.add(new Card(i,j));
       }
     }
-    for(int i = 0; i < 4; i++){
-      collection.add(new Card(i,0);
+    for(int i = 0; i < 4; i++){ //adding color cards numbered 0
+      collection.add(new Card(i,0));
     }
-      for(int j = 13; j < 15; j++){
+      for(int j = 13; j < 15; j++){ //adding wild cards
         collection.add(new Card(5,j));
         collection.add(new Card(5,j));
         collection.add(new Card(5,j));
