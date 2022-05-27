c3969ffd2262cc2bd2d93d8d3b279292266edb12
JefferyTang23 <jtang30@stuy.edu>
2022-05-26 22:38:57 -0400

    updated devlog + proposal

---
+++ Card.java
@@ -0,0 +1,8 @@
+public class Card{
+  private int _color;
+  private int _numberOrSpecialty;
+  public Card(int color, int numberOrSpecialty){
+    _color = color;
+    _numberOrSpecialty = numberOrSpecialty;
+  }
+}
