fef3555db5a77c8c6f2e5e209d8cd237b3ae4d9d
mqiu30 <mqiu30@stuy.edu>
2022-06-03 12:35:53 -0400

    gui>>????

---
+++ v8/Card.java
@@ -0,0 +1,91 @@
+public class Card{
+  private int _color;
+  private int _numberOrSpecialty;
+  private int _secondary; // FOR WILD CARDS
+  public Card(int color, int numberOrSpecialty){
+    _color = color;
+    _numberOrSpecialty = numberOrSpecialty;
+    _secondary = 0;
+  }
+
+  public String getColor() {
+    String color = "";
+    if (_color == 1) {
+      color = "Red";
+    }
+    else if (_color == 2) {
+      color = "Green";
+    }
+    else if (_color == 3) {
+      color = "Yellow";
+    }
+    else if (_color == 4) {
+      color = "Blue";
+    }
+    else {
+      color = "Wild";
+    }
+    return color;
+  }
+
+  public String getNumberOrSpecialty() {
+    String numberOrSpecialty = "";
+    if (_numberOrSpecialty >= 0 && _numberOrSpecialty <= 9) {
+      numberOrSpecialty += _numberOrSpecialty;
+    }
+    else if (_numberOrSpecialty == 10) {
+      numberOrSpecialty = "+2";
+    }
+    else if (_numberOrSpecialty == 11) {
+      numberOrSpecialty = "Reverse";
+    }
+    else if (_numberOrSpecialty == 12) {
+      numberOrSpecialty = "Skip";
+    }
+    else if (_numberOrSpecialty == 13) {
+      numberOrSpecialty = "Regular";
+    }
+    else {
+      numberOrSpecialty = "+4";
+    }
+    return numberOrSpecialty;
+  }
+
+  public void setSecondary(int chosen) { // FOR WHEN PLAYERS CHOOSE A WILD CARD
+    _secondary = chosen;
+  }
+
+  public String getSecondary() {
+    String color = "";
+    if (_secondary == 1) {
+      color = "Red";
+    }
+    else if (_secondary == 2) {
+      color = "Green";
+    }
+    else if (_secondary == 3) {
+      color = "Yellow";
+    }
+    else if (_secondary == 4) {
+      color = "Blue";
+    }
+    return color;
+  }
+
+  public String toString() {
+    String ret = getColor() + " " + getNumberOrSpecialty();
+    if (getColor().equals("Wild")) {
+      ret += " " + getSecondary();
+    }
+    return ret;
+  }
+
+  public static void main(String[] args) {
+    Card i = new Card(1, 4);
+    System.out.println(i);
+    Card am = new Card(1, 12);
+    System.out.println(am);
+    Card hungry = new Card(5, 14);
+    System.out.println(hungry);
+  }
+}
---
+++ v8/Table.java
@@ -0,0 +1,144 @@
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.Scanner;
+
+public class Table {
+  private Stack<Card> placed = new Stack<Card>();
+  private Deck aDeck = new Deck();
+  private int direction = 0;
+  private Player current;
+  private Card top;
+
+  public Table (Player p1, Player p2, Player p3, Player p4) {
+
+    p1.setNext(p2);
+    p2.setNext(p3);
+    p3.setNext(p4);
+    p4.setNext(p1);
+    p1.setPrev(p4);
+    p2.setPrev(p1);
+    p3.setPrev(p2);
+    p4.setPrev(p3);
+    current = p1;
+  }
+
+  public void go(){
+    System.out.println("\nTopmost Card : " + top + "\n");
+
+    System.out.println(current);
+    int placingCard = current.go(top);
+
+    if (placingCard == current.getHandSize()) {
+      current.draw(aDeck.removeFromDeck());
+      current.draw(aDeck.removeFromDeck());
+      System.out.println(current + " drew two cards\n--\n");
+      current = current.nextInLine(direction);
+      go();
+    }
+    else if(current.validateChoice(placingCard, top) == true){
+      placeCard(placingCard);
+      processCard();
+      go();
+    }
+    else{
+      System.out.println("Pick a valid card or else you will be punched by the extremely non violent peace advocates");
+      go();
+    }
+  }
+
+  public Card placeFirst() {
+    placed.push(aDeck.removeFromDeck());
+    top = placed.peek();
+    return top;
+  }
+
+  public Stack<Card> getPlaced() {
+    return placed;
+  }
+
+  // public void endGame(){
+  //   if (current.wonOrNot() == true){
+  //     System.out.println(current.getName() + "has won the game");
+  //     System.out.println("\033[H\033[2J");
+  //   }
+  // }
+
+  public void placeCard(int chosen) {
+    placed.push(current.removeCard(chosen));
+    top = placed.peek();
+  }
+
+  public void processCard(){
+    System.out.println(current.getName() + " placed a " + top + "\n--");
+    if (top.getNumberOrSpecialty().equals("Reverse")){
+      if(direction == 1){
+        direction = 0;
+      }
+      else{
+        direction = 1;
+      }
+      current = current.nextInLine(direction);
+    }
+    else if (top.getNumberOrSpecialty().equals("Skip")){
+      current = current.nextInLine(direction).nextInLine(direction);
+    }
+    else if (top.getNumberOrSpecialty().equals("+2")) {
+      aDeck.replenish(placed);
+      current.nextInLine(direction).draw(aDeck.removeFromDeck());
+      current.nextInLine(direction).draw(aDeck.removeFromDeck());
+      current = current.nextInLine(direction);
+    }
+    else if (top.getNumberOrSpecialty().equals("+4")) {
+      aDeck.replenish(placed);
+      current.nextInLine(direction).draw(aDeck.removeFromDeck());
+      current.nextInLine(direction).draw(aDeck.removeFromDeck());
+      current.nextInLine(direction).draw(aDeck.removeFromDeck());
+      current.nextInLine(direction).draw(aDeck.removeFromDeck());
+
+      current.setSecondary(top);
+      current = current.nextInLine(direction);
+    }
+    else if (top.getColor().equals("Wild")) {
+      current.setSecondary(top);
+      current = current.nextInLine(direction);
+    }
+    else {
+      current = current.nextInLine(direction);
+    }
+
+    Uno();
+
+  }
+
+  public void distribute() {
+    for(int x = 0 ; x < 4 ; x++) {
+      for (int l = 0 ; l < 7 ; l++) {
+        current.draw(aDeck.removeFromDeck());
+      }
+      current = current.nextInLine(direction);
+    }
+  }
+//quntaquinte
+public void Uno(){
+
+  Scanner uNoOuT = new Scanner(System.in);
+  if(current.getHandSize() == 1){
+    long time = System.currentTimeMillis();
+    System.out.println("You have one card");
+    String dhed = uNoOuT.next();
+    long secondTime = System.currentTimeMillis();
+    time = secondTime - time;
+    if(time <= 3000 && (dhed.equals("UNO") || dhed.equals("uno"))){
+      System.out.println("You now have 1 card");
+    }
+    else{
+      System.out.println("You did not type UNO fast enough. You took a card loser.");
+      current.draw(aDeck.removeFromDeck());
+    }
+  }
+}
+  public Card getTop() {
+    return top;
+  }
+
+}
